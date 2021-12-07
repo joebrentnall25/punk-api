af240c103f5d2e57cc212e315977bb11c1e3cf75
Joe Brentnall <joe.brentnall25@gmail.com>
2021-12-07 14:20:39 +0000

    Initial Commit + NO code altered

---
+++ src/components/BeerCard/BeerCard.jsx
@@ -0,0 +1,11 @@
+import "BeerCard.scss";
+
+const BeerCard = props => {
+    return (
+        <>
+            <div className="card__container">
+                
+            </div>
+        </>
+    )
+}
---
+++ src/containers/CardList/CardList.jsx
@@ -0,0 +1,5 @@
+import "./CardList.scss";
+
+const CardList = () => {
+    
+}
